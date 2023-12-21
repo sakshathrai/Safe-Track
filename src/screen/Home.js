a5314060ee0aee5644a8a4cfaf8e06022f13d2cd
PavanDNaik <pawannaik396@gmail.com>
2023-12-22 04:50:56 +0530

    home page initialized

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,11 +1,53 @@
-import React from "react";
-import mobileRechargeLogo from "../image/icons/mobile-recharge.png";
+import React, { useEffect, useState } from "react";
+
+import "../css/home.css";
+function getLevel() {
+  return localStorage.getItem("level");
+}
 function Home() {
+  const [level, setLevel] = useState(getLevel);
+  const services = {
+    1: ["upi", "num", "acc", "recharge", "scan"],
+    2: ["Electricity", "Education", "credit", "water", "cylinder"],
+    3: ["Loan-repay", "donate", "postpaid", "Ticket", "Flight"],
+  };
+  const [showService, setShowServices] = useState(services[level]);
+  const Headings = {
+    upi: "Pay by UPI/QR",
+    num: "Pay to Contacts",
+    acc: "Pay to Bank-Acc",
+    recharge: "Mobile Recharge",
+    scan: "QR Code",
+    Electricity: "Electricity Bill Payment",
+    Education: "Education Fees",
+    credit: "Credit Card PayMent",
+    water: "Water",
+    cylinder: "Book A Cylinder",
+    "Loan-repay": "Loan-repay",
+    donate: "donate",
+    postpaid: "postpaid",
+    Ticket: "Movie ticket",
+    flight: "Flight ticket",
+  };
+  useEffect(() => {
+    setLevel(localStorage.getItem("level"));
+    setShowServices(services[level]);
+  });
   return (
-    <div>
-      <div className="mobile-recharge">
-        <img src={mobileRechargeLogo} alt="Recharge" className="service-icon" />
+    <div className="home-container">
+      <div className="home-header">
+        <button>Account Balance</button>
+        <button>Custom Level</button>
+        <button>Add Card</button>
       </div>
+      {showService.map((v) => {
+        return (
+          <div className="card" key={v}>
+            <i className="fas fa-qrcode"></i>
+            <h2>{Headings[v]}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 }
