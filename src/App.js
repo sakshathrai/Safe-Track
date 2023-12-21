71df0a43c8db69c21209be86694b3c2459da557e
PavanDNaik <pawannaik396@gmail.com>
2023-12-22 00:23:58 +0530

    login design

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Password from "./component/Password";
 import CardPay from "./component/CardPay";
+import Login from "./screen/Login";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/pin" element={<Password />}></Route>
         <Route exact path="/card-pay" element={<CardPay />}></Route>
+        <Route exact path="/login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   );
