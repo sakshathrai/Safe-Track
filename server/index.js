3ab43cdfbef4669aeea26b919c3c0f337db31ada
sakshathrai <sakshathrai2004@gmail.com>
2023-12-21 20:52:56 +0530

    route

--- server/index.js
+++ server/index.js
@@ -3,3 +3,30 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_SECRETE_URI);
 const app = express();
 app.use(express.json());
+
+app.get('/api/setuppin', (req, res) => {
+  
+  res.send();
+});
+
+app.get('/api/signup', (req, res) => {
+  
+    res.send();
+  });
+
+  app.get('/api/verifypin', (req, res) => {
+  
+    res.send();
+  });
+  app.get('/api/profile', (req, res) => {
+  
+    res.send();
+  });
+
+    
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+
+  console.log("Server is started running");
+
+});
