e48bd6441b6fac98cb8d0dc80ccaf6eb8245c69c
sakshathrai <sakshathrai2004@gmail.com>
2023-12-21 21:16:26 +0530

    route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,33 @@
-require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const express = require("express");
 const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_SECRETE_URI);
 const app = express();
 app.use(express.json());
+
+app.get('/api/setuppin', (req, res) => {
+  
+    res.send();
+  });
+  
+  app.get('/api/signup', (req, res) => {
+    
+      res.send();
+    });
+  
+    þÞapp.get('/api/verifypin', (req, res) => {
+    
+      res.send();
+    });
+    app.get('/api/profile', (req, res) => {
+    
+      res.send();
+    });
+  
+      
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+  
+    console.log("Server is started running");
+  
+  });
+  
