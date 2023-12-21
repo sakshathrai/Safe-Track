7e807e5aeeba6b47880f555e7368f496fc63b40c
PavanDNaik <pawannaik396@gmail.com>
2023-12-21 19:40:57 +0530

    pin page value entery

--- src/component/Password.js
+++ src/component/Password.js
@@ -1,40 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/password.css";
 import logo from "../image/logo.jpg";
 function Password() {
-  return <div class="security">
- <div class="main-container">
+  const [pin, setPin] = useState([-1, -1, -1, -1]);
+  const [index, setIndex] = useState(-1);
 
-        <div class="logo-container">
-            <div class="logo">
-                <img src={logo} alt="Logo"/>
-            </div>
+  return (
+    <div className="security">
+      <div className="main-container">
+        <div className="logo-container">
+          <div className="logo">
+            <img src={logo} alt="Logo" />
+          </div>
         </div>
 
         <h1>Welcome to Safe Track</h1>
 
-        <div class="pin-container">
-            <input type="text" class="pin-input" id="pinInput1" readonly/>
-            <input type="text" class="pin-input" id="pinInput2" readonly/>
-            <input type="text" class="pin-input" id="pinInput3" readonly/>
-            <input type="text" class="pin-input" id="pinInput4" readonly/>
+        <div className="pin-container">
+          {pin.map((val, i) => {
+            return (
+              <div
+                key={i}
+                className={`pin-input ${val >= 0 ? "active-input" : ""}`}
+              >
+                {val >= 0 ? val : " "}
+              </div>
+            );
+          })}
         </div>
-        <div class="keypad">
-            <button class="keypad-btn">1</button>
-            <button class="keypad-btn">2</button>
-            <button class="keypad-btn">3</button>
-            <button class="keypad-btn">4</button>
-            <button class="keypad-btn">5</button>
-            <button class="keypad-btn">6</button>
-            <button class="keypad-btn">7</button>
-            <button class="keypad-btn">8</button>
-            <button class="keypad-btn">9</button>
-            <button class="keypad-btn">0</button>
-            <button class="keypad-btn" id="deleteBtn">delete</button>
-            <button class="keypad-btn" id="verifyBtn">verify</button>
+        <div className="keypad">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((val) => {
+            return (
+              <button
+                className="keypad-btn"
+                onClick={() => {
+                  if (index >= 3) return;
+                  pin[index + 1] = val;
+                  setIndex(index + 1);
+                  setPin([...pin]);
+                }}
+              >
+                {val}
+              </button>
+            );
+          })}
+          <button
+            className="keypad-btn"
+            id="deleteBtn"
+            onClick={() => {
+              if (index >= 0) {
+                pin[index] = -1;
+                setPin([...pin]);
+                setIndex(index - 1);
+              }
+            }}
+          >
+            delete
+          </button>
+          <button className="keypad-btn" id="verifyBtn">
+            verify
+          </button>
         </div>
+      </div>
     </div>
-  </div>
+  );
 }
 
 export default Password;
