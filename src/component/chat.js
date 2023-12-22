beca1c771a00ef7d01456b43b7437365b65366f8
sakshathrai <sakshathrai2004@gmail.com>
2023-12-22 12:11:06 +0530

    botmodel2

--- src/component/chat.js
+++ src/component/chat.js
@@ -4,57 +4,18 @@ function ChatBot({ fc }) {
   const [messages, setMessages] = useState([
     { type: "bot", text: "Hello! How can I help you today?" },
   ]);
-  const [userInput, setUserInput] = useState("");
-  const [chatResponse, setChateResponse] = useState([]);
-  const dataSet = {
-    1: ["num", "recharge", "upi", "acc", "scan"],
-    2: ["Electricity", "Education", "credit", "water", "cylinder"],
-    3: ["Loan-repay", "donate", "postpaid", "Ticket", "Flight"],
+  const [userInput, setUserInput] = useState('');
+
+  const sendMessage = () => {
+    if (userInput.trim() !== '') {
+      setMessages([
+        ...messages,
+        { type: 'user', text: userInput.trim() },
+        { type: 'bot', text: 'Hello! How can I help you today?' } // Simulated bot response
+      ]);
+      setUserInput('');
+    }
   };
-  const Headings = {
-    upi: "Pay by UPI/QR",
-    num: "Pay to Contacts",
-    acc: "Pay to Bank-Acc",
-    recharge: "Mobile Recharge",
-    scan: "QR Code",
-    Electricity: "Electricity Bill Payment",
-    Education: "Education Fees",
-    credit: "Credit Card PayMent",
-    water: "Water",
-    cylinder: "Book A Cylinder",
-    "Loan-repay": "Loan-repay",
-    donate: "donate",
-    postpaid: "postpaid",
-    Ticket: "Movie ticket",
-    flight: "Flight ticket",
-  };
-  async function sendMessage() {
-    const postData = { message: userInput };
-    fetch("http://localhost:8080/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(postData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        data.Actions.forEach((v) => {
-          dataSet[1].forEach((val) => {
-            setChateResponse([...chatResponse, val]);
-          });
-          dataSet[2].forEach((val) => {
-            setChateResponse([...chatResponse, val]);
-          });
-          dataSet[3].forEach((val) => {
-            setChateResponse([...chatResponse, val]);
-          });
-        });
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }
 
   return (
     <div className="chat-container">
@@ -67,11 +28,7 @@ function ChatBot({ fc }) {
       <div className="chat-body">
         {messages.map((message, index) => (
           <div key={index} className="message-container">
-            <p
-              className={
-                message.type === "user" ? "user-message" : "bot-message"
-              }
-            >
+            <p className={message.type === 'user' ? 'user-message' : 'bot-message'}>
               {message.text}
             </p>
           </div>
